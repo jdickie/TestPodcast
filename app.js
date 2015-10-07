1b2ca5ff6d250f52fcb30940609aa3c05dcc42c9
Grant Dickie <gdickie@npr.org>
2015-10-07 14:31:30 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var request = require('request'),
+    async = require('async'),
+    program = require('commander'),
+    _ = require('underscore'),
+    moment = require('moment'),
+    package = require('./package.json'),
+    testDownloader = require('./lib/testDownloader.js'),
+    settings = require('./lib/settings.js');
+
+
+async.each(settings.urls, function(url, eachCallback) {
+    try {
+        if (settings.PODTRAC_ON) {
+            url = settings.PODTRAC + url.replace(/^http:\/\//, '');
+        }
+        console.log(url);
+        testDownloader.getBrowserBehaviour((url + settings.PATH), function(err, stats) {
+            if (err) {
+                console.log(moment().format() + " Error:", err);
+            }
+            eachCallback();
+        });
+    } catch (err) {
+        if (err) {
+            console.log(moment().format() + " Error:", err);
+        }
+        eachCallback();
+    }
+});
+
+
