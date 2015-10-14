d55df83467c5e08304471508e6220183e77b13fa
Grant Dickie <gdickie@npr.org>
2015-10-14 11:48:37 -0400

    Fixing logging

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 var request = require('request'),
     async = require('async'),
-    program = require('commander'),
     _ = require('underscore'),
     moment = require('moment'),
     package = require('./package.json'),
     testDownloader = require('./lib/testDownloader.js'),
-    settings = require('./lib/settings.js');
+    settings = require('./lib/settings.js'),
+    process = require('process'),
+    callNum = 0,
+    downloads = 0;
 
 setInterval(function() {
+    callNum++;
+    console.log(moment().format() + " ");
     async.each(settings.urls, function(url, eachCallback) {
         try {
             if (settings.PODTRAC_ON) {
                 url = settings.PODTRAC + url.replace(/^http:\/\//, '');
             }
-            testDownloader.getBrowserBehaviour((url + settings.PATH), function(err, stats) {
+            testDownloader.getBrowserBehaviour((url + settings.PATH), callNum, function(err, stats) {
                 if (err) {
                     console.log(moment().format() + " Error:", err);
                 }
+                downloads++;
                 eachCallback();
             });
         } catch (err) {
@@ -27,6 +32,16 @@ setInterval(function() {
         }
     });
 }, 4320);
+
+process.on('SIGINT', function() {
+    console.log(moment().format(), {
+       downloads : downloads,
+        TotalCalls : callNum
+
+    });
+    
+    process.exit();
+});
 
 
 
